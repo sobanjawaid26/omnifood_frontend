45fbc0852993e613ffc133d7af24d65d5ebe8a98
sobanjawaid26 <sobanjawaid26@gmail.com>
2019-10-07 18:57:32 +0530

    some js added

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,6 +13,19 @@ $(document).ready(function() {
     );
 
     /* Scroll on buttons */
+    $('js--scroll-to-plan').click(function() {
+        $('html, body').animate({ scrollTop: $('.js--section-plan').offset().top}, 1000);
+    });
+
+    $('js--scroll-to-start').click(function() {
+        $('html, body').animate({ scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+    /* Animation on scroll  */
+
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    })
 
 /*  
     var waypoints = $('#handler-first').waypoint(function(direction) {
