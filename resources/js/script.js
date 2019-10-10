ddbebd48cb19333ddb0139df9e89cbada95ace24
sobanjawaid26 <sobanjawaid26@gmail.com>
2019-10-10 11:07:26 +0530

    application completed

--- resources/js/script.js
+++ resources/js/script.js
@@ -25,6 +25,40 @@ $(document).ready(function() {
 
     $('.js--wp-1').waypoint(function(direction){
         $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round')
+        } else{
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round')
+        }
     })
 
 /*  
